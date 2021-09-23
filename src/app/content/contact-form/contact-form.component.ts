f643cee6f3c1cfc450df092d1269282a28d489d0
Arkadiy <ar1kanium@gmail.com>
2021-09-23 23:13:19 +0300

    dynamic inputs and validation, new comm methods

--- src/app/content/contact-form/contact-form.component.ts
+++ src/app/content/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
 
 
@@ -11,24 +11,53 @@ import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
 export class ContactFormComponent {
 
   contactForm: FormGroup
-  communicationMethods = ['WhatsUp', 'Telegram', 'Viber']
+  communicationMethods: string[] = ['WhatsUp', 'Telegram', 'Viber', 'Instagram', 'VK']
   communicationChosenMethod: string = this.communicationMethods[0]
+
   constructor(private fb: FormBuilder, public ref: DynamicDialogRef, public config: DynamicDialogConfig) {
     this.contactForm = this.fb.group({
-      firstName: ['' ,[Validators.required]],
+      firstName: ['', [Validators.required]],
       lastName: '',
-      phoneNumber: ['',[Validators.required]],
+      phoneNumber: ['', [Validators.required]],
+      nickName: this.fb.array([]),
       communicationInfo: '',
     })
   }
+
   submit = () => {
     if (this.contactForm.invalid) {
       this.contactForm.controls['firstName'].markAsDirty()
       this.contactForm.controls['phoneNumber'].markAsDirty()
+      this.getNickNameControls().controls[0]?.markAsDirty()
       return
     }
     this.ref.close({...this.contactForm.value, communicationChosenMethod: this.communicationChosenMethod})
   }
 
+  getNickNameControls(): FormArray {
+    return this.contactForm.controls['nickName'] as FormArray
+  }
 
+  addNickNameField = () => {
+    this.getNickNameControls().push(this.fb.control('', [Validators.required]))
+  }
+
+  deleteNickNameField = () => {
+    this.getNickNameControls().clear()
+  }
+
+
+  defineValidatorsAndFields = () => {
+    if (this.communicationChosenMethod === 'Instagram' || this.communicationChosenMethod === 'VK') {
+      if (!this.getNickNameControls().length) {
+        this.addNickNameField()
+        this.contactForm.controls['phoneNumber'].clearValidators()
+        this.contactForm.controls['phoneNumber'].updateValueAndValidity()
+      }
+    } else {
+      this.deleteNickNameField()
+      this.contactForm.controls['phoneNumber'].setValidators([Validators.required])
+      this.contactForm.controls['phoneNumber'].updateValueAndValidity()
+    }
+  }
 }
